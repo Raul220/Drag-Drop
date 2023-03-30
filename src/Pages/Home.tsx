3c5653897c0df8361af6a7c34845c800d5c7a088
RaulDM220 <rborrego@datamart.cl>
2023-03-29 15:50:56 -1100

    add react-redux

---
+++ src/Pages/Home.tsx
@@ -0,0 +1,14 @@
+
+import * as React from "react";
+import { NavLink } from "react-router-dom";
+
+const Home = () => {
+
+  return (
+    <div>
+      <NavLink to="/execise">Ejercicio</NavLink>
+    </div>        
+  );
+};
+
+export default Home;
